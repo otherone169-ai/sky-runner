393d03d3add5c540c785fbd29228d34608968183
Dev User <dev@example.com>
2025-12-10 22:30:06 +0100

    Add CSP header for AdSense eval support

--- next.config.js
+++ next.config.js
@@ -5,6 +5,19 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
+  headers: async () => {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "script-src 'self' 'unsafe-eval' https://pagead2.googlesyndication.com; connect-src 'self' https://pagead2.googlesyndication.com;",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
